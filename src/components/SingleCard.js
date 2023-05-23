6bd6513f64c995206a43c9d4e6d64604f3d73388
mustafa <m.gonulgul49@gmail.com>
2023-05-23 13:40:04 +0300

    v1 23 05

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import "./SingleCard.scss"
 
-const SingleCard = ({card,coverImage}) => {
+const SingleCard = ({card,handleChoice,flipped,disabled}) => {
+
+
+    const handleClick = ()=>{
+
+      if(!disabled){
+        handleChoice(card)
+
+      }
+       
+      
+    }
+
+    
+
+
   return (
     <div className="card" >
-    <div>
+    <div className={flipped ? "flipped": "" }>
       <img className="front" src={card.src} alt="card front" />
-      <img className="back" src={coverImage} alt="card back" />
+      <img
+       className="back"
+       src={require("../assests/image/cover.png")} 
+       alt="card back" 
+       onClick={handleClick}
+       />
     </div>   
-  </div>
+  </div>  
   )
 }
 
 export default SingleCard
+
+
+// Sub Total :	16739.30$
+// Discount :	2551.69$
+// Tax :	724.95$
+// Total Amount :	14912.56$
+
--- src/components/magicApp/magicApp.js
+++ src/components/magicApp/magicApp.js
@@ -1,30 +1,90 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./magicApp.scss";
-import coverImage from "../../assests/image/cover.png";
 import SingleCard from "../SingleCard";
 
 const MagicApp = () => {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
-  const [choiceOne, setChoiceOne] = useState(null)
-  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false)
+
 
   const cardImages = [
-    { src: require("../../assests/image/helmet-1.png") },
-    { src: require("../../assests/image/potion-1.png") },
-    { src: require("../../assests/image/ring-1.png") },
-    { src: require("../../assests/image/scroll-1.png") },
-    { src: require("../../assests/image/shield-1.png") },
-    { src: require("../../assests/image/shield-1.png") },
+    { src: require("../../assests/image/helmet-1.png" ),matched:false } ,
+    { src: require("../../assests/image/potion-1.png" ),matched:false } ,
+    { src: require("../../assests/image/ring-1.png"   ),matched:false },
+    { src: require("../../assests/image/scroll-1.png" ),matched:false } ,
+    { src: require("../../assests/image/shield-1.png" ),matched:false } ,
+    { src: require("../../assests/image/sword-1.png" ),matched:false } ,
   ];
+
+  
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards);
-    setTurns(0); 
+    setTurns(0);
   };
+
+
+
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+
+  // compare 2 selected cards
+  useEffect( ()=> {
+     
+   
+
+    if(choiceOne && choiceTwo){
+      setDisabled(true) 
+       
+
+      if(choiceOne.src === choiceTwo.src){
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src){
+              return {...card, matched:true}
+            } else {
+               return card 
+            }
+          })
+        })
+        resetTurn()
+
+      }else {
+       setTimeout(() => resetTurn(),1000) 
+
+      }
+
+    }
+
+  },[choiceOne,choiceTwo] )
+
+  // reset choices & increase turn 
+   
+
+
+  // start a new game automaticly
+  useEffect(()=>{
+    shuffleCards()
+  }, [])
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1 )
+    setDisabled(false)
+  }
 
   return (
     <div className="magic-app">
@@ -33,9 +93,16 @@ const MagicApp = () => {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card}/>
+          <SingleCard 
+          key={card.id} 
+          card={card}
+          handleChoice = {handleChoice}
+          flipped = {card === choiceOne ||card === choiceTwo || card.matched }
+          disabled={disabled}
+          />
         ))}
       </div>
+      <p> Turns:{turns} </p>
     </div>
   );
 };
